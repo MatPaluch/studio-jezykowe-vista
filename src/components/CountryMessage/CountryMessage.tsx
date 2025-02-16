b6aa76f638bda95007b86d8ed741b45d8f611411
MatPaluch <xp.mateusz@gmail.com>
2025-02-16 15:34:33 +0100

    europe map

--- src/components/CountryMessage/CountryMessage.tsx
+++ src/components/CountryMessage/CountryMessage.tsx
@@ -1,17 +1,47 @@
 import style from './CountryMessage.module.css';
+import triangle from '../../assets/triangle.svg';
 
 interface countryMessagePropTypes {
   children?: React.ReactNode;
-  text?: string;
   src?: string;
-  className?: string;
+  top?: number;
+  right?: number;
+  bottom?: number;
+  left?: number;
+  flagAlignRight?: boolean;
+  cloudPointerRight?: boolean;
 }
 
-function CountryMessage({ children, text, src, className }: countryMessagePropTypes) {
+function CountryMessage({
+  children,
+  src,
+  top,
+  right,
+  bottom,
+  left,
+  flagAlignRight,
+  cloudPointerRight,
+}: countryMessagePropTypes) {
   return (
-    <div className={`${style.cloud} ${className}`}>
-      <span className={style.message}>{children}</span>
+    <div
+      className={style.cloud}
+      style={{
+        top: top,
+        right: right,
+        bottom: bottom,
+        left: left,
+        flexDirection: flagAlignRight ? 'row-reverse' : 'row',
+      }}
+    >
       <img src={src} width='50' height='30' />
+      <span className={style.message}>{children}</span>
+      <img
+        src={triangle}
+        width={32}
+        height={16}
+        className={style.triangle}
+        style={cloudPointerRight ? { right: 18 } : { left: 18 }}
+      />
     </div>
   );
 }
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,7 +2,10 @@ import { Link } from 'react-router';
 import style from './Hero.module.css';
 import europeMap from '../../assets/europeMap.svg';
 import CountryMessage from '../CountryMessage/CountryMessage';
-import poland from '../../assets/miniflag-poland.svg';
+import poland from '../../assets/mini-flag-poland.svg';
+import germany from '../../assets/mini-flag-germany.svg';
+import uk from '../../assets/mini-flag-uk.svg';
+import spain from '../../assets/mini-flag-spain.svg';
 
 function Hero() {
   const handle = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -25,8 +28,17 @@ function Hero() {
         </div>
         <div className={style.rightSide}>
           <img width='734' height='456' src={europeMap} className={style.svgEuropeMap} />
-          <CountryMessage src={poland} className={style.poland}>
-            Miło cię widzieć!
+          <CountryMessage src={poland} top={112} right={106} flagAlignRight={true}>
+            Cześć!
+          </CountryMessage>
+          <CountryMessage src={uk} top={106} left={206} cloudPointerRight={true}>
+            Hello!
+          </CountryMessage>
+          <CountryMessage src={germany} top={160} right={244}>
+            Hallo!
+          </CountryMessage>
+          <CountryMessage src={spain} bottom={132} left={64} cloudPointerRight={true}>
+            ¡Hola!
           </CountryMessage>
         </div>
       </div>
