b399f18f4aa3c585a77dca9c30ad89ff8b9f1445
MatPaluch <xp.mateusz@gmail.com>
2025-04-30 16:19:55 +0200

    nav_update

--- src/components/Header/Login/Login.tsx
+++ src/components/Header/Login/Login.tsx
@@ -1,9 +1,17 @@
+import Button from '../../Button/Button';
 import styles from './Login.module.css';
+import send from '../../../assets/send.svg';
 
 function Login() {
   return (
-    <div className={styles.buttonWrapper}>
-      <button className={styles.loginButton}>Zaloguj</button>
+    <div className={styles.loginContainer}>
+      <div className={styles.buttonWrapper}>
+        <button className={styles.loginButton}>Zaloguj</button>
+      </div>
+      <Button>
+        <img src={send} alt="sendIcon" width={24} height={24} />
+        Zajęcia próbne
+      </Button>
     </div>
   );
 }
