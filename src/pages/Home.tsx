b7e97d35003364ff7fd4288c7b6f2341dd70799f
MatPaluch <xp.mateusz@gmail.com>
2025-08-11 20:08:20 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import AboutUs from '../components/AboutUs/AboutUs';
 import Languages from '../components/Languages/Languages';
 import Faq from '../components/FAQ/Faq';
 import Contact from '../components/Contact/Contact';
-import Form from '../components/Form/Form';
+// import Form from '../components/Form/Form';
 import ClickToAction from '../components/ClickToAction/ClickToAction';
 import { useEffect } from 'react';
 import { useLocation } from 'react-router';
