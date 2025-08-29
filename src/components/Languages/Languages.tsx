81febb0f76911e82414b6dcdfac91001bb8ef234
Mateusz Paluszkiewicz <150561295+MatPaluch@users.noreply.github.com>
2025-08-29 09:58:07 +0000

    Languages

--- src/components/Languages/Languages.tsx
+++ src/components/Languages/Languages.tsx
@@ -1,11 +1,9 @@
 import en from '../../assets/mini-flag-uk.svg';
 import de from '../../assets/mini-flag-germany.svg';
 import es from '../../assets/mini-flag-spain.svg';
-import chevron from '../../assets/chevron_right.svg';
 import pointer_polygon from '../../assets/pointer_polygon.svg';
 
 import style from './Languages.module.css';
-import Button from '../Button/Button';
 
 function Languages() {
   return (
